fa86b04031d255677c4ca214c7852b97bef4a36f
Strophox <strophox@gmail.com>
2023-12-01 08:13:52 +0100

    solve day01

---
+++ day01.hs
@@ -0,0 +1,29 @@
+import Data.List
+
+day = "01"
+
+main = do
+  putStrLn ("Opening Advent calendar door "<>day<>" where")
+  txt <- init <$> readFile (day<>".txt")
+  putStrLn ("  part 1 = "<>show (solve1 txt))
+  putStrLn ("  part 2 = "<>show (solve2 txt))
+
+solve1 :: String -> Int
+solve1 = sum . map (read . (\ds -> [head ds,last ds]) . lineDigits) . lines
+  where lineDigits =  filter (`elem`['0'..'9'])
+
+solve2 :: String -> Int
+solve2 = sum . map (read . (\ds -> [head ds,last ds]) . lineDigits) . lines
+  where lineDigits = concatMap suffixMatches . inits
+        suffixMatches str = [d | (token,d)<-dict, token`isSuffixOf`str]
+        dict = zip  ["0","1","2","3","4","5","6","7","8","9","zero","one","two","three","four","five","six","seven","eight","nine"] (cycle ['0'..'9'])
+
+{-NOTE old, parses "oneight" as only "1"
+lineDigits2 :: String -> String
+lineDigits2 = fst . foldl step ([],[])
+  where step (ds,cs) c = maybe (ds,cs++[c]) (\d -> (ds++[d],[])) (dict`parseMatch`(cs++[c]))
+        dict = zip ["0","1","2","3","4","5","6","7","8","9","zero","one","two","three","four","five","six","seven","eight","nine"] (cycle ['0'..'9'])
+
+parseMatch :: [(String,Char)] -> String -> Maybe Char
+parseMatch dict str = listToMaybe [d | (token,d)<-dict, token`isSuffixOf`str]
+-}
