e7056e26544a47ec9d5cac977ff5aee49f2dafe5
Strophox <strophox@gmail.com>
2023-12-07 08:59:40 +0100

    tweak 04golf

--- day04_golfed.hs
+++ day04_golfed.hs
@@ -1,12 +1,12 @@
 import Data.List (intersect)
 import Control.Arrow (first,(&&&))
 
-main = (solve1 &&& solve2) <$> readFile "04.txt" >>= print
-
-solve1 = sum . map ((2^) . pred) . filter (>0) . parse
-
-solve2 = fold . map ((,) 1) . parse
-  where fold [] = 0
-        fold ((cs,v):rest) = cs + fold (zipWith (first . (+)) (replicate v cs ++ repeat 0) rest)
+main = (solve1 &&& solve2) <$> parse <$> readFile "04.txt" >>= print
 
 parse = map (length . uncurry intersect . break (=="|") . words . drop 9) . lines
+
+solve1 = sum . map ((2^) . pred) . filter (>0)
+
+solve2 = cascade . map ((,) 1)
+  where cascade [] = 0
+        cascade ((cs,v):rest) = cs + cascade (zipWith (first . (+)) (replicate v cs ++ repeat 0) rest)
