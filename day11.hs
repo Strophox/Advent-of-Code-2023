78c64f959a8bb754b48660f83a3a5ef6df3a84ad
Strophox <strophox@gmail.com>
2023-12-11 09:37:27 +0100

    refactor day11

--- day11.hs
+++ day11.hs
@@ -7,19 +7,20 @@ main = let day = "11" in do
   putStrLn ("  part 1 = "<>show (solve1 txt))
   putStrLn ("  part 2 = "<>show (solve2 txt))
 
-solve1 = pairDistances . index . expand . transpose . expand . lines
-  where expand = foldr (\a b -> if all (=='.') a then a:a:b else a:b) []
-        index rows = [(x,y) | (y,row)<-zip [0..] rows,
-                              (x,chr)<-zip [0..] row, chr=='#']
+solve1 = (`div`2) . sum . distances
+       . (\rows -> [(x,y) | (y,row)<-zip [0..] rows, (x,c)<-zip [0..] row, c=='#'])
+       . expand . transpose
+       . expand . lines
+  where expand = foldr (\x xs -> if all (=='.') x then x:x:xs else x:xs) []
 
-
-solve2 = pairDistances . (\(rows,_) -> [(u,v) | row<-rows, (u,(v,c))<-row, c=='#'])
+solve2 = (`div`2) . sum . distances
+       . (\(rows,_) -> [(x,y) | row<-rows, (x,(y,c))<-row, c=='#'])
        . expandedIndex . (transpose *** transpose)
        . expandedIndex . (id &&& id) . lines
-  where expandedIndex (xs,os) = (fst $ foldr build ([],0) (zip xs os), os)
-        build (x,o) (xs',i) = ((zip [i,i..] x):xs',
-                               if all (=='.') o then i+1000000 else i+1)
+  where expandedIndex :: ([[a]],[[Char]]) -> ([[(Int,a)]],[[Char]])
+        expandedIndex (ys,xs) = (fst $ foldr index ([],0) (zip ys xs), xs)
+        index (y,x) (iys,i) = (zip [i,i..] y : iys,
+                               if all (=='.') x then i+1000000 else i+1)
 
-pairDistances :: [(Int,Int)] -> Int
-pairDistances cs = sum allDistances `div` 2
-  where allDistances = (\(x,y) (x',y') -> abs (x-x') + abs (y-y')) <$> cs <*> cs
+distances :: [(Int,Int)] -> [Int]
+distances cs = (\(x,y) (x',y') -> abs (x-x') + abs (y-y')) <$> cs <*> cs
