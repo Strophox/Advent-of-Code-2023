397be955252a0286268adf38a803f6b017b7d503
Strophox <strophox@gmail.com>
2023-12-11 09:57:57 +0100

    final cleanup day11 (fr ong)

--- day11.hs
+++ day11.hs
@@ -7,20 +7,19 @@ main = let day = "11" in do
   putStrLn ("  part 1 = "<>show (solve1 txt))
   putStrLn ("  part 2 = "<>show (solve2 txt))
 
-solve1 = (`div`2) . sum . distances
-       . (\rows -> [(x,y) | (y,row)<-zip [0..] rows, (x,c)<-zip [0..] row, c=='#'])
-       . expand . transpose
-       . expand . lines
-  where expand = foldr (\x xs -> if all (=='.') x then x:x:xs else x:xs) []
+solve1 = (`div`2) . sum . distances . coords
+       . expand . transpose . expand . lines
+  where
+    coords rows = [(x,y) | (y,row)<-zip [0..] rows, (x,c)<-zip [0..] row, c=='#']
+    expand = foldr (\x xs -> if all (=='.') x then x:x:xs else x:xs) []
 
-solve2 = (`div`2) . sum . distances
-       . (\(rows,_) -> [(x,y) | row<-rows, (x,(y,c))<-row, c=='#'])
-       . expandedIndex . (transpose *** transpose)
-       . expandedIndex . (id &&& id) . lines
-  where expandedIndex :: ([[a]],[[Char]]) -> ([[(Int,a)]],[[Char]])
-        expandedIndex (ys,xs) = (fst $ foldr index ([],0) (zip ys xs), xs)
-        index (y,x) (iys,i) = (zip [i,i..] y : iys,
-                               if all (=='.') x then i+1000000 else i+1)
+solve2 = (`div`2) . sum . distances . coords'
+       . indexExpand . (transpose *** transpose) . indexExpand . (id &&& id) . lines
+  where
+    coords' (_,rows) = [(x,y) | row<-rows, (x,(y,c))<-row, c=='#']
+    indexExpand (xs,zs) = (xs, snd $ foldr index (0,[]) (zip xs zs))
+    index (x,z) (i,izs) = (if all (=='.') x then i+1000000 else i+1,
+                           zip [i,i..] z : izs)
 
 distances :: [(Int,Int)] -> [Int]
 distances cs = (\(x,y) (x',y') -> abs (x-x') + abs (y-y')) <$> cs <*> cs
