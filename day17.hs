61d97425c5f07bc9cdc30a3bd4aecd2d0a1823bd
Strophox <strophox@gmail.com>
2023-12-30 23:17:19 +0100

    solve day17

--- day17.hs
+++ day17.hs
@@ -4,11 +4,6 @@ import Data.Array
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
-import Debug.Trace
-logx str x = trace (str<>" :: "<>show x) x
-logf str f x = trace (str<>" :: "<>show x<>" -> _") (f x)
-logfx str f x = trace (str<>" :: "<>show x<>" -> "<>show (f x)) (f x)
-
 main = let day = "17" in do
   txt <- readFile (day<>".txt")
   putStrLn ("Opening Advent calendar door "<>day<>" where")
@@ -47,20 +42,25 @@ calcDistance grid muls = dijkstra neighbors [(I2 0 0,Hrzt),(I2 0 0,Vert)] ((snd 
               let dist = sum [grid ! (n .* dv .+ coord) | n<-[s,2*s..m]] ]
 
 parse :: String -> Array Int2 Int
-parse txt = listArray (I2 0 0, I2 n n) [read [c] | c<-txt, c/='\n']
-  where n = length (lines txt) - 1
+parse txt = listArray (I2 0 0, I2 y x) [read [c] | c<-txt, c/='\n']
+  where (y,x) = ((length . lines) txt - 1, (length . head . lines) txt - 1)
 
---dijkstra :: Ord a => (a -> [(Int,a)]) -> [a] -> (a -> Bool) -> Int--M.Map a Int
+dijkstra :: Ord a => (a -> [(Int,a)]) -> [a] -> (a -> Bool) -> Int--M.Map a Int
 dijkstra neighbors starts isStop = run M.empty (S.fromList [(0,n) | n<-starts]) where
-  --run dists pqueue = logf "run" (uncurry run') (dists,pqueue)
   run dists pqueue
     | S.null pqueue = error "no solution found"--dists
     | node`M.member`dists = run dists rest
-    | isStop node = dist--M.insert node dist dists
-    | otherwise   = run dists' pqueue'
+    | isStop node   = dist--M.insert node dist dists
+    | otherwise     = run dists' pqueue'
     where Just ((dist,node),rest) = S.minView pqueue
-          --(dist,node) = logx"c"(dist',node')
-          newNodes = [(dist+d,n) | (d,n)<-neighbors node, n`M.notMember`dists]
           dists'  = M.insert node dist dists
-          pqueue' = foldr S.insert rest newNodes
---(if fst node == I2 2 10 then \x -> trace ("nbrs :: "<>show (dists M.! (I2 4 10,Vert),node,n,dists M.! node,x)) x else id)$
+          pqueue' = foldr S.insert rest [(dist+d,n) | (d,n)<-neighbors node, n`M.notMember`dists]
+
+{-NOTE man. what the f
+
+import Debug.Trace
+logx str x = trace (str<>" :: "<>show x) x
+logf str f x = trace (str<>" :: "<>show x<>" -> _") (f x)
+logfx str f x = trace (str<>" :: "<>show x<>" -> "<>show (f x)) (f x)
+
+(if fst node == I2 2 10 then \x -> trace ("nbrs :: "<>show (dists M.! (I2 4 10,Vert),node,n,dists M.! node,x)) x else id)$-}
