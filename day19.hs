52216d03c962cb107b3d9ee643fdfc1c0ae2e043
Strophox <strophox@gmail.com>
2024-01-11 21:20:45 +0100

    solve day19 pt1, get day19 pt2 to compile

--- day19.hs
+++ day19.hs
@@ -13,12 +13,13 @@ main = let day = "19" in do
   putStrLn ("  part 1 = "<>show (solve1 txt))
   putStrLn ("  part 2 = "<>show (solve2 txt))
 
-solve1 txt = sum $ sum <$> filter (workflows !> "in") parts
+solve1 txt = sum . map rating . filter (workflows!>"in") $ parts
   where (workflows,parts) = parse1 txt
+        rating = sum
 
-solve2 txt = undefined{-sum $ size <$> (workflows !> "in") (0,4000)
-  where workflows = parse2 txt
-        size r = undefined-}
+solve2 txt = sum . map size . (rangeWorkflows!>"in") $ replicate 4 (0,4000)
+  where rangeWorkflows = parse2 txt
+        size = product . map (\(a,b) -> b-a+1)
 
 type Part = [Int]
 type Workflow = Part -> Bool
@@ -27,43 +28,46 @@ parse1 :: String -> ([(String, Workflow)], [Part])
 parse1 = (fixAssoc . map perWorkflow . head &&& map perPart . last) . splitOn [""] . lines
   where perPart = map (read . last . splitOn "=") . splitOn "," . init
         perWorkflow str = let [name,steps'] = splitOn "{" str
-          in (name, (makeWorkflow . splitOn "," . init) steps')
+          in (name, (buildWorkflow . splitOn "," . init) steps')
 
-makeWorkflow :: [String] -> [(String, Workflow)] -> Workflow
-makeWorkflow rules workflows part = foldr check (call (last rules) part) (init rules)
-  where check rule rest = let
-            [(f:c:ds),dest] = splitOn ":" rule
-            field = part !! (zip "xmas" [0..] !> f)
-            cmp = case c of '>' -> (>) ; '<' -> (<)
-            num = read ds
-          in if field`cmp`num then call dest part else rest
-        call s = case s of
-          "A" -> const True ; "R" -> const False ; w -> workflows !> w
+buildWorkflow :: [String] -> [(String, Workflow)] -> Workflow
+buildWorkflow rules workflows part = foldr check (call (last rules) part) (init rules)
+  where check :: String -> Bool -> Bool
+        check rule next = let
+          [f:c:num,fn] = splitOn ":" rule
+          field = zip "xmas" part !> f
+          cmp = case c of '>' -> (>); '<' -> (<)
+          n = read num
+          in if field`cmp`n then call fn part else next
+        call fn = case fn of "A" -> pure True; "R" -> pure False; str -> workflows!>str
 
-{-type PartRange = [(Int,Int)]
-type RangeWorkflow = PartRange -> PartRange
+type PartRange = [(Int,Int)]
+type RangeWorkflow = PartRange -> [PartRange]
 
 parse2 :: String -> [(String, RangeWorkflow)]
-parse2 = fixAssoc . map perWorkflow . head . splitOn [""] . lines
+parse2 = fixAssoc . map perWorkflow . takeWhile (not.null) . lines
   where perWorkflow str = let [name,steps'] = splitOn "{" str
-          in (name, (makeRangeWorkflow . splitOn "," . init) steps')
+          in (name, (buildRangeWorkflow . splitOn "," . init) steps')
 
-makeRangeWorkflow :: [String] -> [(String, RangeWorkflow)] -> RangeWorkflow
-makeRangeWorkflow rules workflows partr = foldr check (call (last rules) partr) (init rules)
-  where check rule rest = let
-            [(f:c:ds),dest] = splitOn ":" rule
-            field = part !! (zip "xmas" [0..] !> f)
-            cmp = case c of '>' -> (>) ; '<' -> (<)
-            num = read ds
-          in if field`cmp`num then call dest partr ++ rest partr''
-        call s = case s of
-          "A" -> const True ; "R" -> const False ; w -> workflows !> w-}
+buildRangeWorkflow :: [String] -> [(String, RangeWorkflow)] -> RangeWorkflow
+buildRangeWorkflow rules workflows partRng = foldr branch (call (last rules)) (init rules) partRng
+  where branch :: String -> (PartRange -> [PartRange]) -> PartRange -> [PartRange]
+        branch rule next partRng = if any (\(l,r) -> r < l) partRng then [] else let
+          [f:c:num,fn] = splitOn ":" rule
+          n = read num
+          (thenRng,elseRng) = let
+            (as,(l,r):cs) = splitAt (zip "xmas" [0..] !> f) partRng
+            (b1,b0) = case c of '>' -> ((n+1,r),(l,n)); '<' -> ((l,n-1),(n,r))
+            in (as++b1:cs, as++b0:cs)
+          in call fn thenRng ++ next elseRng {-FIXME-}
+        call fn = case fn of "A" -> (:[]); "R" -> const []; str -> workflows!>str
 
-{-NOTE c.f.
-fix :: ((a -> b) -> (a -> b)) -> (a -> b)
-fix f = f (fix f)  -}
-fixAssoc :: [(a, [(a,b -> c)] -> b -> c)] -> [(a,b -> c)]
-fixAssoc cs = map (fmap ($ fixAssoc cs)) cs
-
+infixl 9 !>
 (!>) :: (Eq a)=> [(a,b)] -> a -> b
 xys !> x = maybe (error "not in assoc list") id (lookup x xys)
+
+fixAssoc :: [(a, [(a, b -> c)] -> b -> c)] -> [(a, b -> c)]
+fixAssoc cs = map (fmap ($ fixAssoc cs)) cs
+{-NOTE c.f.
+fix :: ((a -> b) -> (a -> b)) -> (a -> b)
+fix f = f (fix f)-}
